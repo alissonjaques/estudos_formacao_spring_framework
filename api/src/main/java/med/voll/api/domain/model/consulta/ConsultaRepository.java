205e4e2e1688397a4b555adb596475f7d13862c8
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-04-22 09:29:40 -0300

    Adicionando CRUD básico de consultas na aplicação

---
+++ api/src/main/java/med/voll/api/domain/model/consulta/ConsultaRepository.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain.model.consulta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+}
