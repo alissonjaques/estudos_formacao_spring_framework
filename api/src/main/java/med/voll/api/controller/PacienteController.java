f8765feef8361110e659c77fe3629935a86542b5
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2023-05-22 14:04:35 -0300

    Adição da documentação na API

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.model.paciente.DadosListagemPaciente;
 import med.voll.api.domain.model.paciente.Paciente;
@@ -17,6 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
     @Autowired
     private PacienteRepository repository;
